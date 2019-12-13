abe980ccebbb22e29c8d81b2aa27f17ff4c3f8b1
brunoalbubrito <brunoalbuquerquebrito@outlook.com>
2019-12-13 14:49:46 -0300

    Criando base do projeto API

---
+++ cartorio-web/src/main/java/br/com/docket/cartorioweb/CartorioWebApplication.java
@@ -0,0 +1,13 @@
+package br.com.docket.cartorioweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartorioWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CartorioWebApplication.class, args);
+	}
+
+}
---
+++ cartorio-web/src/test/java/br/com/docket/cartorioweb/CartorioWebApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.docket.cartorioweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartorioWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
